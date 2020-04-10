f2bc076a1c1b2f6bd22b0560ce681ff492be1f52
megasoft909@gmail.com <megasoft909@gmail.com>
2020-04-10 23:45:48 +0530

    commiting angular website

---
+++ tesolo-website/tesolo-tech-web/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {AppRoutingModule, routingComponent } from './app.routing.module';
+import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    routingComponent,
+    PageNotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
