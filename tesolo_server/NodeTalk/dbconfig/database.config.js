79d30a381e1c98efbb3d9f3d29ff250c93cef3a3
megasoft909@gmail.com <megasoft909@gmail.com>
2020-05-15 12:15:04 +0530

    Remove the now ignored directory "./dbconfig"

--- tesolo_server/NodeTalk/dbconfig/database.config.js
+++
@@ -1,5 +0,0 @@
-    module.exports = {
-        // url: 'mongodb://localhost:27017/easy-notes'
-        url: 'mongodb+srv://tesolotech:tesolotech@tesolotech-dmwtl.mongodb.net/tesolotech?retryWrites=true&w=majority'
-    }
-
